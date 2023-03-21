cef957fa813525d826bae0ee85295937da12bfa9
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-21 21:26:14 +0100

    Hibernate

---
+++ src/main/java/com/hb01/annotation/Student01.java
@@ -0,0 +1,66 @@
+package com.hb01.annotation;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity //!!! @Entity ile bu sinifin DB de bir tabloya karsilik gelmesini sagliyoruz., student01
+@Table(name = "t_student01") //opsiyoneldir, tablo adi konulmazsa class adini tablo adi olarak default alir.
+public class Student01 {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private int grade;
+
+
+    //Constructor
+
+    public Student01() {
+    }
+
+    public Student01(int id, String name, int grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    //Getter - Setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    //Methods
+
+    @Override
+    public String toString() {
+        return "Student01{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
