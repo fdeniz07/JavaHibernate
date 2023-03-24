11070c51a1d642754c5f1dcea3fdb3f78116f454
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-24 21:56:24 +0100

    Hibernate

---
+++ src/main/java/com/hb03/uni_onetoone/RunnerFetch03.java
@@ -0,0 +1,35 @@
+package com.hb03.uni_onetoone;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch03 {
+
+    public static void main(String[] args) {
+
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student03.class).addAnnotatedClass(Diary03.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Student03 student = session.get(Student03.class, 1001);
+
+        Diary03 diary = session.get(Diary03.class, 101);
+
+        System.out.println(student);
+        System.out.println("********************");
+        System.out.println(diary);
+        System.out.println("********************");
+        System.out.println(diary.getStudent());
+
+
+        tx.commit();
+        session.close();
+        sf.close();
+
+
+    }
+}
