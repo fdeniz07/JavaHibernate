1a5845333bbc07e37f2894be8a647085876e774a
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-25 21:44:43 +0100

    Hibernate

--- src/main/java/com/hb04/bi_onetoone/RunnerSave04.java
+++ src/main/java/com/hb04/bi_onetoone/RunnerSave04.java
@@ -15,6 +15,7 @@ public class RunnerSave04 {
         student1.setName("Ahmet");
         student1.setGrade(50);
 
+
         Student04 student2 = new Student04();
         student2.setId(1002);
         student2.setName("Mehmet");
@@ -29,11 +30,14 @@ public class RunnerSave04 {
         diary1.setId(101);
         diary1.setName("A diary");
         diary1.setStudent(student1);
+        //student1.setDiary(diary1); // mappedBy, Diary'deki student' i getirdigi icin id null basacaktir.
 
         Diary04 diary2 = new Diary04();
         diary2.setId(102);
         diary2.setName("B diary");
         diary2.setStudent(student2);
+        //student1.setDiary(diary2);
+
 
         Diary04 diary3 = new Diary04();
         diary3.setId(103);
