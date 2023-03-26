199376dfe1d4d87d88c4f55ef316bb143723dc67
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-26 19:37:14 +0200

    Hibernate

---
+++ src/main/java/com/tpe/annotations/RunnerFetch01.java
@@ -0,0 +1,56 @@
+package com.tpe.annotations;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RunnerFetch01 {
+
+    public static void main(String[] args) {
+
+
+        Configuration cfg=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Developer01.class);
+        SessionFactory sf=cfg.buildSessionFactory();
+        Session session=sf.openSession();
+        Transaction tx=session.beginTransaction();
+
+        Developer01 dev=session.get(Developer01.class,1);
+        System.out.println(dev);
+
+        //1-sql ile tüm datayı çekiniz.
+        String sqlQuery1="SELECT * FROM t_developer01";
+        List<Object[]> resultList=session.createSQLQuery(sqlQuery1).getResultList();
+        System.out.println("------SQL----------");
+        for (Object[] r:resultList ){
+            System.out.println(Arrays.toString(r));
+        }
+
+        //2-hql ile tüm datayı çekiniz.
+        String hqlQuery1="FROM Developer01";
+        List<Developer01> devList=session.createQuery(hqlQuery1, Developer01.class).getResultList();
+        System.out.println("------HQL----------");
+        devList.forEach(t-> System.out.println(t));
+
+        //***ÖDEV***
+        //3-hql ile ismi 'Shrek' olan datayı çekiniz.
+        //4-hql ile emaili 'jack@gmail.com' olan datayı çekiniz.
+        //5-hql ile branch i backend olan datanın ismini getiriniz.
+
+
+
+
+        tx.commit();
+        session.close();
+        sf.close();
+
+
+
+    }
+
+
+
+}
