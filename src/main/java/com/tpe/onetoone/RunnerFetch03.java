251946a9750795f1f91af5da757158fb0aeae1ad
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-27 19:57:47 +0200

    Hibernate Practice

---
+++ src/main/java/com/tpe/onetoone/RunnerFetch03.java
@@ -0,0 +1,53 @@
+package com.tpe.onetoone;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch03 {
+    public static void main(String[] args) {
+
+        Configuration cfg=new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Developer03.class).addAnnotatedClass(Computer.class);
+        SessionFactory sf=cfg.buildSessionFactory();
+        Session session=sf.openSession();
+        Transaction tx=session.beginTransaction();
+
+        //1)id=1 olan developerın tüm bilgilerini getiriniz.
+
+        Developer03 dev1=session.get(Developer03.class,1);
+        System.out.println("-----------------1-------------------");
+        System.out.println(dev1);
+        System.out.println("------------------1------------------");
+
+        //2)id=33 olan computerın tüm bilgilerini getiriniz.
+        Computer computer=session.get(Computer.class,33);
+        System.out.println("-----------------2-------------------");
+        System.out.println(computer);
+        System.out.println("------------------2------------------");
+
+
+        //3)id=22 olan computerı kullanan developerın tüm bilgilerini getiriniz.
+        Computer computer2=session.get(Computer.class,22);
+        System.out.println("-----------------3-------------------");
+        System.out.println(computer2.getDeveloper());//uni-directional olsaydı computerdan dev e ulaşamazdık.
+        System.out.println("-----------------3-------------------");
+
+
+        //4)id=2 olan developerın kullandığı computerın tüm bilgilerini getiriniz.
+        Developer03 dev2=session.get(Developer03.class,2);
+        System.out.println("-----------------4-------------------");
+        System.out.println(dev2.getComputer());
+        System.out.println("-----------------4-------------------");
+
+        //***ÖDEV***
+        //tüm developerların ismini ve kullandıkları computerın markasını yazdırınız.
+
+        tx.commit();
+        session.close();
+        sf.close();
+
+
+    }
+}
