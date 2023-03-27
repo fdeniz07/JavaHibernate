d51b056609f872732044e95d0f65a6fce02d23e2
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-27 22:46:41 +0200

    Hibernate

---
+++ src/main/java/com/hb05/RunnerFetch05.java
@@ -0,0 +1,42 @@
+package com.hb05;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RunnerFetch05 {
+
+    public static void main(String[] args) {
+
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student05.class).addAnnotatedClass(University.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+
+        //!!! get metodu
+//        Student05 student = session.get(Student05.class, 1001);
+//        System.out.println(student);
+//        System.out.println(student.getUniversity());
+
+
+        //!!! HQL ile
+        String hqlQuery1 = "FROM Student05 s WHERE s.university.id=1";
+        List<Student05> resultList1 = session.createQuery(hqlQuery1, Student05.class).getResultList();
+        resultList1.forEach(System.out::println);
+
+//        resultList1.forEach(oa -> {
+//            System.out.println(Arrays.toString(s));
+//        });
+
+
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+}
