c6f3c2dce088a0d489dfa6cd1a6801972e8e8aec
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-31 01:26:49 +0200

    Hibernate

---
+++ src/main/java/com/hb09/fetchtypes/RunnerFetch09.java
@@ -0,0 +1,33 @@
+package com.hb09.fetchtypes;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch09 {
+
+    public static void main(String[] args) {
+
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Student09.class).addAnnotatedClass(Book09.class);
+
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+          Student09 student1 = session.get(Student09.class,1002);
+//        System.out.println(student1.getBookList());
+
+        Book09 book1 = session.get(Book09.class,101);
+        System.out.println(book1.getStudent());
+
+        for (Book09 book : student1.getBookList()){ //LAZZY
+            System.out.println(book);
+        }
+
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+}
