33d54ba2756c90fa4369e24c2a0be803eb15b60c
Fatih Deniz <deniz.fatih@hotmail.de>
2023-03-30 02:56:56 +0200

    Hibernate

---
+++ src/main/java/com/hb08/manytomany/Book08.java
@@ -0,0 +1,46 @@
+package com.hb08.manytomany;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Book08 {
+
+    @Id
+    private int id;
+    private String name ;
+
+    @ManyToMany(mappedBy = "bookList")
+    private List<Student08> students = new ArrayList<>();
+
+
+    //!!! Getter-Setter
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Student08> getStudents() {
+        return students;
+    }
+    public void setStudents(List<Student08> students) {
+        this.students = students;
+    }
+
+    //!!! toString()
+    @Override
+    public String toString() {
+        return "Book08{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
