8d0ce284f4381831b9ccd1015289e67d3b24bb77
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-01 02:13:42 +0200

    Hibernate

--- src/main/java/com/hb05/manytoone/RunnerFetch05.java
+++ src/main/java/com/hb05/manytoone/RunnerFetch05.java
@@ -1,18 +1,18 @@
-package com.hb05;
+package com.hb05.manytoone;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class RunnerFetch05 {
 
     public static void main(String[] args) {
 
-        Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student05.class).addAnnotatedClass(University.class);
+        Configuration con = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student05.class).addAnnotatedClass(University.class);
 
         SessionFactory sf = con.buildSessionFactory();
         Session session = sf.openSession();
