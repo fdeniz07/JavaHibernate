73a0d640464f651267139f329993c3c64c0ed588
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-03 22:17:56 +0200

    Hibernate

---
+++ src/main/java/com/hb13/get_load/RunnerFetch13.java
@@ -0,0 +1,24 @@
+package com.hb13.get_load;
+
+/*
+    get() ---> gerçek nesneyi döndürür ,
+                nesne yoksa null döner
+                nesnenin olduğundan emin değilseniz get() kullanın
+                dönen nesneyi hemen kullanacaksam get() kullanılmalı
+    load() --> proxy nesne döndürür, gerçek nesnenin gölgesi ,
+                nesne yoksa exception fırlatır
+                dönen nesne üzerinde delete yapılacaksa kullanılabilir
+
+
+ */
+
+
+public class RunnerFetch13 {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
